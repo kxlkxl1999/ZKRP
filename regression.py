af2d846295b5a48358f19281f11b9683f6d6d07c
kxlkxl2019 <kangxinlai@126.com>
2022-12-09 03:17:59 +0800

    PC

--- regression.py
+++ regression.py
@@ -242,21 +242,24 @@ def eval(y1, y2, yh1, yh2, datatype='CR', method='RMSE'):
     elif method == 'IOR':
         ior = []
         for i in range(n):
-            if (y_l[i] - yh_l[i]) * (y_u[i] - yh_u[i]) >= 0:
+            if (y_l[i] - yh_l[i]) * (y_u[i] - yh_u[i]) > 0:
                 if abs(y_c[i] - yh_c[i]) >= (abs(y_r[i])+abs(yh_r[i])):
                     ior.append(0)
                 else:
                     ior.append(min(abs(y_l[i] - yh_u[i]), abs(y_u[i] - yh_l[i])) / (2 * abs(y_r[i])))
             else:
-                ior.append(min(abs(y_r[i]), abs(yh_r[i])) / abs(y_r[i]))
+                if y_r[i] == 0:
+                    return 1
+                else:
+                    ior.append(min(abs(y_r[i]), abs(yh_r[i])) / abs(y_r[i]))
         return np.mean(ior)
 
     elif method == 'DC':
         # return 1 - np.sum((y_r - yh_r) ** 2) / np.sum((y_r - np.mean(y_r)) ** 2)
-        # return np.corrcoef(y_r, yh_r)[0][1] ** 2 /2 + np.corrcoef(y_c, yh_c)[0][1] ** 2 /2
+        return np.corrcoef(abs(y_r), abs(yh_r))[0][1] ** 2 /2 + np.corrcoef(y_c, yh_c)[0][1] ** 2 /2
         # return np.corrcoef(y_c, yh_c)[0][1] ** 2
     # elif method == 'DC' and datatype == 'LU':
-        return (1 - np.sum((y_l - yh_l) ** 2) / np.sum((y_l - np.mean(y_l)) ** 2))/2 + (1 - np.sum((y_u - yh_u) ** 2) / np.sum((y_u - np.mean(y_u)) ** 2))/2
+    #     return (1 - np.sum((y_l - yh_l) ** 2) / np.sum((y_l - np.mean(y_l)) ** 2))/2 + (1 - np.sum((y_u - yh_u) ** 2) / np.sum((y_u - np.mean(y_u)) ** 2))/2
     else:
         raise Exception('Wrong method. method can only be chosen from \'RMSE\', \'LU\', \'NHD\', \'DC\'. ')
 
