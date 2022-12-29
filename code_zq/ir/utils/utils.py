0c907c970a8460cae12c60004b08f3fa6397b399
kxlkxl1999 <kangxinlai@126.com>
2022-12-29 21:44:03 +0800

    Laptop

--- code_zq/ir/utils/utils.py
+++ code_zq/ir/utils/utils.py
@@ -16,6 +16,56 @@ def simplelstsq(x, y):
     # print(np.array([reg.intercept_[0], reg.coef_[0, 0]]))
 
 
+def medianregression(x, y, positivebta=False):
+    """
+    # CVXOPT linear programming standard form
+    # min c^Tx
+    # subject to Gx <=h, Ax=b
+
+    # median regression as linear programming
+    # min \sum_{i}(ei+ + ei-)
+    # subject to ei+, ei- >=0
+    # ei+ - ei- = yi - (a+b*xi)
+    """
+    n = x.shape[0]
+    # equality constraints
+    A = np.vstack([np.ones_like(x), x, np.eye(n), -np.eye(n)]).T
+    b = y
+    # the first two elements corresponds to the regression coefficient
+    # the next n elements corresponds to the positive part of residual
+    # the last n elements corresponds to the negative part of residual
+    c = np.concatenate([np.zeros(2), np.ones(2 * n)])
+
+    # inequality constraints
+    if positivebta:
+        G = -np.eye(2 + 2 * n)
+        h = np.zeros(2 + 2 * n)
+    else:
+        G = np.concatenate(
+            [
+                np.zeros((2 * n, 2)),
+                np.concatenate([np.diag(-np.ones(n)), np.zeros((n, n))], 0),
+                np.concatenate([np.zeros((n, n)), np.diag(-np.ones(n))], 0),
+            ],
+            1,
+        )
+        h = np.zeros(2 * n)
+
+    # solving the model
+    sol = solvers.lp(
+        matrix(c),
+        matrix(G),
+        matrix(h),
+        matrix(A),
+        matrix(b),
+        options={"show_progress": False},
+    )
+    x = sol["x"]
+
+    beta = x[:2]
+    return beta
+
+
 def constrained_simplelstsq(x, y):
     """
     The objective function is qudratic in beta0 and beta1
@@ -29,163 +79,3 @@ def constrained_simplelstsq(x, y):
     sol = solvers.qp(P, q, G, h, options={"show_progress": False})
 
     return np.array(sol["x"])
-
-
-# performance metrics
-def evaluation(yc, yr, hatyc, hatyr, criterion="RMSE"):
-    """
-    performance metric for interval linear regression
-
-    :param yc: numpy-array; center of response interval
-    :param yr: numpy-array; range of response interval
-    :param hatyc: numpy-array; predicted center of response interval
-    :param hatyr: numpy-array; predicted range of response interval
-    :param criterion:
-    'RMSE': root-mean-square error
-    'MHD': mean Hausdorff Distance
-    'IOR': interval overlap ratio
-    'DC': determination coefficient
-
-    :return:
-    'RMSE': (float, float); root-mean-square error of central and range
-    'MHD': float; mean Hausdorff Distance of real and estimated data
-    'IOR': float;
-    'DC': float;
-    """
-
-    if criterion == "RMSE":
-        rmsel = np.sqrt(pow(yc - yr - hatyc + hatyr, 2).mean())
-        rmseu = np.sqrt(pow(yc + yr - hatyc - hatyr, 2).mean())
-        # rmsel = np.sqrt(pow(yc - hatyc, 2).mean())
-        # rmseu = np.sqrt(pow(yr - hatyr, 2).mean())
-        return rmsel, rmseu
-    elif criterion == "MHD":
-        return (
-            hausdorff_dist_vec(
-                np.vstack([yc, yr]).T, np.vstack([hatyc, hatyr]).T, cr=True
-            ).mean(),
-            hausdorff_dist_vec(
-                np.vstack([yc, yr]).T, np.vstack([hatyc, hatyr]).T, cr=True
-            ).mean(),
-        )
-    elif criterion == "IOR":
-        ior = []
-        for i in range(n):
-            if (y_l[i] - yh_l[i]) * (y_u[i] - yh_u[i]) >= 0:
-                if abs(y_c[i] - yh_c[i]) >= (abs(y_r[i]) + abs(yh_r[i])):
-                    ior.append(0)
-                else:
-                    ior.append(
-                        min(abs(y_l[i] - yh_u[i]), abs(y_u[i] - yh_l[i]))
-                        / (2 * abs(y_r[i]))
-                    )
-            else:
-                ior.append(min(abs(y_r[i]), abs(yh_r[i])) / abs(y_r[i]))
-        return np.mean(ior)
-
-    elif criterion == "DC":
-        Rc2 = np.mean((hatyc - yc.mean()) ** 2) / np.mean((yc - yc.mean()) ** 2)
-        Rr2 = np.mean((hatyr - yr.mean()) ** 2) / np.mean((yr - yr.mean()) ** 2)
-        return Rc2, Rr2
-
-    else:
-        raise Exception(
-            "Wrong criterion. Criterion can only be chosen from 'RMSE', 'MHD', 'IOR', 'DC'. "
-        )
-
-
-def show(data, path, result=None):
-    """
-    show the distribution of interval data
-    :param data: numpy[n,2]: interval data, each line indicates an interval
-    :param path: path of saving fig
-    :param result: dict: the frechet_mean of the interval dataset
-    :return: none
-    """
-    n = np.shape(data)[0]
-    y = range(n)
-    for i in range(n):
-        plt.plot(data[i, :], np.array([y[i], y[i]]), alpha=1, color="black")
-    if result:
-        plt.plot(
-            result["interval"],
-            np.array([round(n / 2), round(n / 2)]),
-            alpha=1,
-            color="red",
-            linewidth=2,
-        )
-    plt.title("plot of interval")  # 折线图标题
-    plt.xlabel("Range")  # x轴标题
-    plt.ylabel("Number")  # y轴标题
-    plt.grid(ls="-.")  # 绘制背景线
-    plt.tight_layout()
-    plt.savefig(path)
-    plt.show()
-
-
-def show2(x, y, path, Cov=None):
-    """
-    show the distribution od two interval datasets
-    :param x: numpy[n,2]: interval data, each line indicates an interval
-    :param y: numpy[n,2]: interval data, each line indicates an interval
-    :param path: path of saving fig
-    :param Cov: double: frechet_cov of x and y
-    :return: none
-    """
-    n = np.shape(x)[0]
-    y1 = np.array(range(n))
-    y2 = y1 + 0.1
-    a = None
-    b = None
-    for i in range(n):
-        (a,) = plt.plot(
-            x[i, :], np.array([y1[i], y1[i]]), alpha=1, color="red", label="X"
-        )
-        (b,) = plt.plot(
-            y[i, :], np.array([y2[i], y2[i]]), alpha=1, color="blue", label="Y"
-        )
-    if Cov:
-        plt.title(
-            "plot of " + r"$X_1$" + " and " + r"$X_2$" + " with Cov %.4f" % Cov
-        )  # 折线图标题
-    else:
-        plt.title("plot of " + r"$X_1$" + " and " + r"$X_2$")
-    plt.xlabel("Range")  # x轴标题
-    plt.ylabel("Number")  # y轴标题
-    plt.grid(ls="-.")  # 绘制背景线
-    plt.legend(loc="best", handles=[a, b])
-    plt.tight_layout()
-    plt.savefig(path)
-    plt.show()
-
-
-def show3(x, y, samples=25, path=None, real=None, Cor=None):
-    np.random.seed(0)
-    n = np.shape(x)[0]
-    plt.figure(dpi=200, figsize=(5, 5))
-    for i in np.random.choice(n, samples, replace=False):
-        plt.fill_between(
-            x[i, :],
-            np.array([y[i, 1], y[i, 1]]),
-            np.array([y[i, 0], y[i, 0]]),
-            alpha=0.25,
-            linewidth=0,
-            color="steelblue",
-        )
-        if real is not None:
-            plt.scatter(real[i, 0], real[i, 1], alpha=1, color="black", s=2)
-
-    if Cor is not None:
-        plt.title(
-            "plot of " + r"$X_1$" + " and " + r"$X_2$" + " with Cor %.4f" % Cor
-        )  # 折线图标题
-    else:
-        plt.title("plot of " + r"$X_1$" + " and " + r"$X_2$")
-    plt.xlabel(r"$X_1$")  # x轴标题
-    plt.ylabel(r"$X_2$")  # y轴标题
-    plt.grid(ls="-.")  # 绘制背景线
-    # plt.legend(loc='best', handles=[a, b])
-    plt.tight_layout()
-    if path is not None:
-        plt.savefig(path)
-    plt.show()
