0a885945f1dc98696e09f1abe34ae78f3a8a9989
kxlkxl2019 <kangxinlai@126.com>
2023-02-22 14:01:36 +0800

    PC

--- R/DataGeneration.R
+++ R/DataGeneration.R
@@ -69,3 +69,85 @@ data_generation_outlier1 <- function(n, a, b, c, d, e, f, g, h, i, j, seed, outl
     yu = y.C + y.R
   ))
 }
+
+data_generation_outlier2 <- function(n, a, b, c, d, e, f, g, h, i, j, seed, outlierType="central", alpha=0.1){
+    set.seed(seed)
+    x.C  = c()   # center point of x
+    x.R  = c()
+    beta0 = 0
+    beta1 = 0
+    betastar = 0
+    x.C = runif(n, a, b)
+    beta1 = runif(1, c, d)
+    betastar = runif(1, g, h)
+    beta0 = runif(n, c, d)
+    epsilon = runif(n, e, f)
+    epsilonr = runif(n, i, j)
+    y.C = beta0 + x.C * beta1 + epsilon
+    x.R = x.C * betastar + epsilonr
+    y.R = y.C * betastar + epsilonr
+    n1 = floor(n*alpha)
+    sampled.index = sample(1:n,n1)
+    
+    if(outlierType == "central")
+        y.C[sampled.index] = 1000
+    else if(outlierType == "range")
+        y.R[sampled.index] = 1000
+    else if(outlierType == "central and range")
+    {
+        y.C[sampled.index] = 1000
+        y.R[sampled.index] = 1000
+    }
+    
+    return(list(
+        xc = x.C,
+        xr = x.R,
+        yc = y.C,
+        yr = y.R,
+        xl = x.C - x.R,
+        xu = x.C + x.R,
+        yl = y.C - y.R,
+        yu = y.C + y.R
+    ))
+}
+
+data_generation_outlier3 <- function(n, a, b, c, d, e, f, g, h, i, j, seed, outlierType="central", alpha=0.1){
+    set.seed(seed)
+    x.C  = c()   # center point of x
+    x.R  = c()
+    beta0 = 0
+    beta1 = 0
+    betastar = 0
+    x.C = runif(n, a, b)
+    beta1 = runif(1, c, d)
+    betastar = runif(1, g, h)
+    beta0 = runif(n, c, d)
+    epsilon = runif(n, e, f)
+    epsilonr = runif(n, i, j)
+    y.C = beta0 + x.C * beta1 + epsilon
+    x.R = x.C * betastar + epsilonr
+    y.R = y.C * betastar + epsilonr
+    n1 = floor(n*alpha)
+    sampled.index = sample(1:n,n1)
+    
+    if(outlierType == "central")
+        y.C[sampled.index] = runif(n1, 6, 12)
+    else if(outlierType == "range")
+        y.R[sampled.index] = runif(n1, 2, 4)
+    else if(outlierType == "central and range")
+    {
+        y.C[sampled.index] = runif(n1, 6, 12)
+        y.R[sampled.index] = runif(n1, 2, 4)
+    }
+    
+    return(list(
+        xc = x.C,
+        xr = x.R,
+        yc = y.C,
+        yr = y.R,
+        xl = x.C - x.R,
+        xu = x.C + x.R,
+        yl = y.C - y.R,
+        yu = y.C + y.R
+    ))
+}
